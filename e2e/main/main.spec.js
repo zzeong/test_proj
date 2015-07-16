501fef11063d7efa1d729af47c820ff3e343c169
jong hoon <hagfish1210@gmail.com>
2015-07-16 18:11:09 +0900

    My favorite beer is Volfas Angelman

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -9,7 +9,7 @@ describe('Main View', function() {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!'); // phantom.js 코드 (코드로 브라우저를 테스트한다. not gui)
+    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!'); // phantom.js 코드 (코드로 브라우저를 테스트한다. not gui 실제로 브라우저를 띄워서 검사를 하는데 창이 없을뿐.)
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
