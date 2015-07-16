69d522da0488be45da5f2432b6c92f65608edf93
jong hoon <hagfish1210@gmail.com>
2015-07-16 15:11:49 +0900

    얘는 모르는게 없다

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -9,7 +9,7 @@ describe('Main View', function() {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
+    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!'); // phantom.js 코드 (코드로 브라우저를 테스트한다. not gui)
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
